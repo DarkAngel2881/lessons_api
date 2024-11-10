bc509ce3b7bd5600a0107e6923fea27909e235c9
matteo2881 <matteo2881@gmail.com>
2024-11-10 18:49:57 +0000

    added default route

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
 @app.route('/lessons')
-def hello_world():
+def lessons_json():
     from uni_lessons_scraper import get_lessons
     events = get_lessons(False)
     return events
